d5dd6332e85f09c9e754b34f3ef454ac4e71bb3a
drizzutojr <drizzutojr@Dannys-MacBook-Pro.local>
2017-12-01 01:25:43 -0600

    made strings same length

--- m4a_analyzer.py
+++ m4a_analyzer.py
@@ -5,6 +5,8 @@ for file in os.listdir("Songs"):
   if file != ".DS_Store":
     f = open('Songs/'+file, 'rb')
     cleaned_up = str(f.read()).split("mdat!",1)[1]
+    cleaned_up = (cleaned_up[:2658998]) if len(cleaned_up) > 2658998 else cleaned_up
+    print(len(cleaned_up))
 
     if min_len > len(cleaned_up):
       min_len =  len(cleaned_up)
