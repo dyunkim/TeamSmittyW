b52a020184ca334deeec92b47b4287238612706d
David Kim <dykim@smu.edu>
2017-11-30 22:56:39 -0600

    notebook and finished data maker

--- datacollector.py
+++ datacollector.py
@@ -1,6 +1,7 @@
 import requests
 import billboard_miner
-api_token = "Bearer BQB7Ojx2hlfuMpAJkVExFA5hdcfv1MR1k_jp7qUE_Np142Y40k-j-Ed8Cs7eeNKIp0bU0t7BzoxrqSYsoYNGqtVXE06bgW2VjHyEy5VGvKk6pN1n5kN8wzgRb7zoSLJuo3gOHn-rALAMayM-vPg"
+import csv
+api_token = "Bearer BQA0CEOidRj4hYYlmluSx-vHOu6o_hW34EK7N1NYIN0JpJhJBHqzJagva7AkSzE3JS4G0yoMEHzHfJMU9pd5UHRPYesGCfcx2xb9G4NWZhuTuZ63gMRClBrQw9OKgVg1nt8v0QZG1fMxmgg__ik"
 
 def getSongPops(song_list):
     base_url = "https://api.spotify.com/v1/search"
@@ -8,29 +9,47 @@ def getSongPops(song_list):
 
     popularity_list = []
     for x in song_list:
-        song_name = x["song_name"].replace(" ", "+")
-        query_string = "%s %s" % (x["artist"], song_name)
+        song_name = x["song"]
+        if "Featuring" in x["artist"]:
+            artist = x["artist"][:x["artist"].index("Featuring")-1]
+        else:
+            artist = x["artist"]
+        query_string = "%s %s" % (artist, song_name)
         query = {"q": query_string, "type": "track", "limit": 1}
         r = requests.get(base_url, params=query, headers=headers)
         if r.status_code != requests.codes.ok:
-            print("Failed to Download Track: %s" % x["song_name"])
+            print("Failed to Download Track: %s - %s" % (x["song"], artist))
             print("Failed with Error Code: %d" % r.status_code)
         else:
             song_dict = {}
-            json = r.json()["tracks"]["items"][0]
-            song_dict["popularity"] = json["popularity"]
-            song_dict["song_name"] = json["name"]
-            artist_list = []
-            for x in json["artists"]:
-                artist_list.append(x["name"])
-            song_dict["artists"] = artist_list
-            popularity_list.append(song_dict)
+            if len(r.json()["tracks"]["items"]) < 1:
+                print("Can't find song: %s" % x["song"])
+                print("Query: %s" % r.url)
+            else:
+                json = r.json()["tracks"]["items"][0]
+                song_dict["popularity"] = json["popularity"]
+                song_dict["song_name"] = json["name"]
+                artist_list = []
+                for x in json["artists"]:
+                    artist_list.append(x["name"])
+                song_dict["artists"] = artist_list
+                popularity_list.append(song_dict)
     return popularity_list
 
+def dictToCSV(dictionaryList):
+    with open('data.csv', 'w') as file:
+        fieldnames = ['song_name', 'artists', 'popularity']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in dictionaryList:
+            writer.writerow(i)
 
 songs = billboard_miner.miner("https://www.billboard.com/charts/year-end/2015/hot-rap-songs")
 # print(songs)
 
 temp = [{"artist": "drake", "song_name": "hotline bling"}]
-pop_list = getSongPops(temp)
-print(pop_list)
+pop_list = getSongPops(songs)
+for x in pop_list:
+    print(x)
+dictToCSV(pop_list)
+print("done")
