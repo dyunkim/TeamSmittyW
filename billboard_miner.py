e1e93b08126e9c0b7c2e82faf6420dea13deb719
drizzutojr <drizzutojr@Dannys-MacBook-Pro.local>
2017-11-30 21:01:16 -0600

    removed whitespace from miner file

--- billboard_miner.py
+++ billboard_miner.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as b_s
 import re
 import pprint
 
+
 def miner(url):
 
   song_list = []
@@ -17,11 +18,8 @@ def miner(url):
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', item.text).rstrip().strip().split('\n')
 
-    #print(type(cleantext))
-
     cleantext = [x for x in cleantext if x != "" ]
     cleantext = [x for x in cleantext if x != " " ]
-
 
     song_list.append(cleantext)
 
@@ -33,6 +31,7 @@ def miner(url):
     song_list_dict.append(song_dict)
 
   return song_list_dict
+
 
 if __name__ == "__main__":
   songs = miner("https://www.billboard.com/charts/year-end/2015/hot-rap-songs")
